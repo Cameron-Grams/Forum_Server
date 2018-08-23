4eac9a0b0a0b36c1d4c45b6e1e6cb174c1662f59
cameron <cameron.grams.2@gmail.com>
2018-08-22 21:43:25 -0400

    work through the minimal returns for an endpoint

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require( 'express' );
 const app = express();
+const bodyparser = require( 'body-parser' ); 
+
+app.use( bodyparser.json() );
 
 app.get( '/', ( req, res ) => {
     res.send( "Hello World\n" );
 } );
+
+app.post( '/', ( req, res ) => {
+    console.log( "with req: ", req.body );
+    console.log( "+++++++++++++++++++++++++++++++++++++++" );
+    res.json( { body: "received " + req.body.text });
+})
 
 app.listen( 3000, () => console.log( "Listening on 3000" ) );
 
