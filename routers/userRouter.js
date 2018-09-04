3e9c3a0e0a93e620a969e63220b181313d537de4
cameron <cameron.grams.2@gmail.com>
2018-09-03 21:43:20 -0400

    keep working the min...

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,7 +7,11 @@ const router = express.Router();
 router.route( '/users/login' )
     .post( ( req, res ) => {
         console.log( "In user router with: ", req.body );
-        res.json( { message: "Good test" } );
+        res.json( { 
+            message: "Good test",
+            userName: "Lizzy",
+            userId: 1
+        } );
     });
 
 
